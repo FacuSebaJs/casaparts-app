eab3aff90b19304bea8e5940036b238fa3c3c976
claudio <claudio.skulski@ionic.io>
2025-08-19 10:20:41 -0300

    sin ssr

--- src/main.server.ts
+++
@@ -1,9 +0,0 @@
-import 'zone.js/node';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideServerRendering(), ...appConfig.providers]
-});
